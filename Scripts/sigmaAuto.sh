cc4ac832a9a45ef3a0ca8b53e6edd73c77a70e97
jvm <jacobmahto148@gmail.com>
2019-07-27 15:11:22 +0530

    update

--- Scripts/sigmaAuto.sh
+++ Scripts/sigmaAuto.sh
@@ -1,6 +1,8 @@
 #/bin/bash
-USER='sigmaeduindia'
-PASSWORD='mohitsirsigma1'
+USER='username'
+PASSWORD='password'
+read -p "Enter Username : " USER
+read -p "Enter password : " PASSWORD
 
 pftp -nv files.000webhost.com<<HULULU
 quote USER $USER
